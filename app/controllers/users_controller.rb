dc0eb6a575c43b6895e868705ca56d225ab6da3b
mwraich <mwraich28@gmail.com>
2016-02-03 16:35:57 -0500

    Add controllers, models, and views for new users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,36 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path, notice: 'Welcome to Sit Yourself Down, Please'
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attirbutes(user_params)
+      redirect_to users_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
-  has_many :reservations 
+  has_secure_password
+  has_many :reservations
 end
