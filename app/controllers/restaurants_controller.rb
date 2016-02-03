6fadab3bf2871c6ea7251b6fc2c4bb599bc37bed
Ann Marie Doyle <am.doyle@yahoo.ca>
2016-02-03 17:52:05 -0500

    Add routes and controller for restaurant

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,50 @@
+class RestaurantsController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def edit
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def update
+    @restaurant = Restaurant.find(params[:id])
+
+    if @restaurant.update_attributes(restaurant_params)
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :edit
+  end
+
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+
+    @restaurant.destroy
+    redirect_to owner_path(@owner.id)
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name,:address, :phone_number,
+    :picture, :description, :capacity, :opening_time, :closing_time)
+  end
+end
