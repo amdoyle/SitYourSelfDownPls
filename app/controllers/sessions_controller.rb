e7d304c0d67ff56685c12f17cd347ffcbac06e55
tanner466 <tanner466@gmail.com>
2016-02-03 17:13:29 -0500

    Add login authentication for owners

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    if params[:user_type]=="owner"
+      owner = Owner.find_by(email: params[:email])
+      if owner && owner.authenticate(params[:password])
+        session[:owner_id] = owner.id
+        redirect_to owner_path(owner.id), notice: "Logged in as owner!"
+      else
+        render "new", notice: "Login is as owner failed"
+      end
+    elsif params[:user_type]=="user"
+
+    end
+
   end
 
   def destroy
