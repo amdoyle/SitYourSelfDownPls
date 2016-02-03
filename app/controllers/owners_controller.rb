a49d9c24b6757603425a0832cb49bff13db272e5
tanner466 <tanner466@gmail.com>
2016-02-03 17:01:51 -0500

    Implement owner and add session controller

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -6,10 +6,30 @@ class OwnersController < ApplicationController
   def create
     @owner = Owner.new(owner_params)
     if @owner.save
-      redirect_to new_owner_path, notice: "Signed up as owner!"
+      redirect_to owner_path(@owner.id), notice: "Signed up as owner!"
     else
       render "new"
     end
+  end
+
+  def edit
+    @owner = Owner.find(params[:id])
+  end
+
+  def show
+    @owner = Owner.find(params[:id])
+  end
+
+  def update
+    @owner = Owner.find(params[:id])
+    
+
+    if @owner.update_attributes(owner_params)
+      redirect_to owner_path(@owner.id)
+    else
+      render :edit
+    end
+
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  resources :sessions, only: [:new, :create, :destroy]
+
   resources :owners, only: [:new, :create, :edit, :show, :update]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
