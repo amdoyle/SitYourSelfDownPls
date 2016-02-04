6f586534721f42a03dd494fe7e0506bac2d7b1a9
SweetNAMese <t.lyna.nguyen@gmail.com>
2016-02-04 14:33:55 -0500

    add reservation nesting under restaurant. Form for new reservation created

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,12 +1,20 @@
 class ReservationsController < ApplicationController
   def new
-    
+    @restaurant = Restaurant.find(params[:id])
+    @reservation = Reservation.new
   end
 
   def create
+    @reservation = Reservation.new(reservation_params)
+    if @reservation.save
+      redirect_to reservation_path(@reservation.id), notice: "Reservation has been created!"
+    else
+      render "new"
+    end
   end
 
   def show
+    @reservation = Reservation.find(params[:id])
   end
 
 end
