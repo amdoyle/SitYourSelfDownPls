684a1e463dc7bcda48b2115662067ebbcf2ec987
mwraich <mwraich28@gmail.com>
2016-02-04 14:47:20 -0500

    Added in categories new and edit pages

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   resources :restaurants
 
-  resources :categories, only: [:show, :index]
+  resources :categories
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
