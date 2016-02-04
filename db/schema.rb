63ce6cfbf40629261f69836c82d96ea93a65dcda
mwraich <mwraich28@gmail.com>
2016-02-04 14:29:17 -0500

    Add categories controller, index, and factories migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160203202733) do
+ActiveRecord::Schema.define(version: 20160204161831) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "factories", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "owners", force: :cascade do |t|
