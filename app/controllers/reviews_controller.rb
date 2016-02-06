2fa02bea8ad47d8a7e81d3a7557c15d42e3c759e
Ann Marie Doyle <am.doyle@yahoo.ca>
2016-02-05 21:27:21 -0500

    Add create review function

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,17 +1,19 @@
 class ReviewsController < ApplicationController
 
+  before_action :load_restaurant
+
   def show
     @review = Review.find(params[:id])
   end
 
   def create
     @review = @restaurant.reviews.build(review_params)
-    @review.user = current_user
+
 
     if @review.save
-      redirect_to restaurant_path, notice:"Your review has been successfully saved"
+      redirect_to restaurants_path, notice:"Your review has been successfully saved"
     else
-      render "restaurant/show"
+      render restaurants_path
     end
   end
 
@@ -25,7 +27,7 @@ class ReviewsController < ApplicationController
       params.require(:review).permit(:comment, :rating, :user_id, :restaurant_id)
     end
 
-    def load_product
+    def load_restaurant
       @restaurant = Restaurant.find(params[:restaurant_id])
     end
 end
