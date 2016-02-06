6c7cb8a175f6f2dbfc531f8da6286f1fa0f2c237
Ann Marie Doyle <am.doyle@yahoo.ca>
2016-02-05 19:42:14 -0500

    set up controller for reviews and altered resturants for reviews

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -29,6 +29,7 @@ class RestaurantsController < ApplicationController
   def show
     @restaurant = Restaurant.find(params[:id])
     @reservation = @restaurant.reservations.build
+    @review = @restaurant.reviews.build
   end
 
   def edit
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,12 +1,31 @@
 class ReviewsController < ApplicationController
 
   def show
+    @review = Review.find(params[:id])
   end
 
   def create
+    @review = @restaurant.reviews.build(review_params)
+    @review.user = current_user
+
+    if @review.save
+      redirect_to restaurant_path, notice:"Your review has been successfully saved"
+    else
+      render "restaurant/show"
+    end
   end
 
   def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
   end
-  
+
+  private
+    def review_params
+      params.require(:review).permit(:comment, :rating, :user_id, :restaurant_id)
+    end
+
+    def load_product
+      @restaurant = Restaurant.find(params[:restaurant_id])
+    end
 end
